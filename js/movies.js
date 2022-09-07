1b8db24070058ee2c257000562f7b985ecbe1cc6
oscar <oscarchan96@outlook.com>
2022-09-06 21:05:36 -0500

    feat: agregado nuevas vistas para ver los detalles de la pelicula

--- js/movies.js
+++ js/movies.js
@@ -1,14 +1,14 @@
-// api key  from TMDB 
+// api key  from TMDB
 const api = "api_key=5f633841a9fdd684a028b2f38006f591";
-// base url of the site 
+// base url of the site
 const base_url = "https://api.themoviedb.org/3";
 // url
 const final_url = base_url + "/discover/movie?sort_by=popularity.desc&" + api;
-// img url 
+// img url
 const img_url = "https://image.tmdb.org/t/p/original";
 // ACTORES
-var arr_cast_global = []
-// requests for movies data 
+var arr_cast_global = [];
+// requests for movies data
 const requests = {
     fetchPopular: `${base_url}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&${api}`,
     fetchTrending: `${base_url}/trending/all/week?${api}&language=en-US`,
@@ -19,7 +19,7 @@ const requests = {
     fetchRomanceMovies: `${base_url}/discover/movie?${api}&with_genres=35`,
     fetchDocumentaries: `${base_url}/discover/movie?${api}&with_genres=27`,
 };
-// used to truncate the string 
+// used to truncate the string
 function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
 }
@@ -27,7 +27,7 @@ function truncate(str, n) {
 // Movies
 let movies = [];
 
-// top rated 
+// top rated
 fetch(requests.fetchTrending)
     .then((res) => res.json())
     .then((data) => {
@@ -42,22 +42,25 @@ fetch(requests.fetchTrending)
         const row_posters = document.createElement("div");
         row_posters.className = "row__posters";
         row.appendChild(row_posters);
+
+        //Funcion para limitar el numero de peliculas a 15 dentro de carrousel
         data.results.forEach((movie, index) => {
             //console.log(movie);
             if (index < 15) {
-                const poster = document.createElement("img");
-                poster.className = "row__posterLarge";
-                var s2 = movie.id;
-                poster.id = s2;
-                poster.src = img_url + movie.poster_path;
-                row_posters.appendChild(poster);
-                let mov = movie.title != null ? movie.title : movie.name;
-                movies.push(mov);
-                const button = document.createElement("div");
-                button.classList.add('row-col', 'mb-1');
-                button.innerHTML = `<button type="button" class="row__button btn btn-light" onclick="getInfo(${movie.id})" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                </button>`;
-                row_posters.appendChild(button)
+                const movie_card = document.createElement("div");
+                movie_card.className = "row__posterLarge";
+                const { poster_path, id } = movie;
+                const body_card = document.createElement("div");
+                body_card.classList.add("movie");
+
+                body_card.innerHTML = `
+                                        <img src=${img_url + poster_path}>
+                                        <div class="overview">
+                                        <button type="button" class="know-more" onclick="getInfo(${id})" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
+                                        </div>
+                `;
+                movie_card.appendChild(body_card);
+                row_posters.appendChild(movie_card);
             }
         });
     });
@@ -80,14 +83,20 @@ fetch(requests.fetchActionMovies)
         data.results.forEach((movie, index) => {
             //console.log(movie);
             if (index < 15) {
-                const poster = document.createElement("img");
-                poster.className = "row__poster";
-                var s2 = movie.id;
-                poster.id = s2;
-                poster.src = img_url + movie.backdrop_path;
-                row_posters.appendChild(poster);
-                let mov = movie.title != null ? movie.title : movie.name;
-                movies.push(mov);
+                const movie_card = document.createElement("div");
+                movie_card.className = "row__poster";
+                const { backdrop_path, id } = movie;
+                const body_card = document.createElement("div");
+                body_card.classList.add("movieGenres");
+
+                body_card.innerHTML = `
+                                        <img src=${img_url + backdrop_path}>
+                                        <div class="overviewGenres">
+                                        <button type="button" class="know-moreGenres" onclick="getInfo(${id})" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
+                                        </div>
+                `;
+                movie_card.appendChild(body_card);
+                row_posters.appendChild(movie_card);
             }
         });
     });
@@ -110,14 +119,20 @@ fetch(requests.fetchComedyMovies)
         data.results.forEach((movie, index) => {
             //console.log(movie);
             if (index < 15) {
-                const poster = document.createElement("img");
-                poster.className = "row__poster";
-                var s2 = movie.id;
-                poster.id = s2;
-                poster.src = img_url + movie.backdrop_path;
-                row_posters.appendChild(poster);
-                let mov = movie.title != null ? movie.title : movie.name;
-                movies.push(mov);
+                const movie_card = document.createElement("div");
+                movie_card.className = "row__poster";
+                const { backdrop_path, id } = movie;
+                const body_card = document.createElement("div");
+                body_card.classList.add("movieGenres");
+
+                body_card.innerHTML = `
+                                        <img src=${img_url + backdrop_path}>
+                                        <div class="overviewGenres">
+                                        <button type="button" class="know-moreGenres" onclick="getInfo(${id})" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
+                                        </div>
+                `;
+                movie_card.appendChild(body_card);
+                row_posters.appendChild(movie_card);
             }
         });
     });
@@ -139,27 +154,30 @@ fetch(requests.fetchHorrorMovies)
         data.results.forEach((movie, index) => {
             //console.log(movie);
             if (index < 15) {
-                const poster = document.createElement("img");
-                poster.className = "row__poster";
-                var s2 = movie.id;
-                poster.id = s2;
-                poster.src = img_url + movie.backdrop_path;
-                row_posters.appendChild(poster);
-                let mov = movie.title != null ? movie.title : movie.name;
-                movies.push(mov);
+                const movie_card = document.createElement("div");
+                movie_card.className = "row__poster";
+                const { backdrop_path, id } = movie;
+                const body_card = document.createElement("div");
+                body_card.classList.add("movieGenres");
+
+                body_card.innerHTML = `
+                                        <img src=${img_url + backdrop_path}>
+                                        <div class="overviewGenres">
+                                        <button type="button" class="know-moreGenres" onclick="getInfo(${id})" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
+                                        </div>
+                `;
+                movie_card.appendChild(body_card);
+                row_posters.appendChild(movie_card);
             }
         });
     });
 
 // Funcion para obtener los datos de cada pelicula y mostrarlos en el elemento Modal
 function getInfo(movieID) {
-
-    const carousel = document.getElementById("carousel");
-    const peliculas = document.querySelector('.pelicula');
-    const fila = document.querySelector('.contenedor-carousel');
-    const flechaIzquierda = document.getElementById('flecha-izquierda');
-    const flechaDerecha = document.getElementById('flecha-derecha');
-    console.log(fila.DOCUMENT_NODE);
+    const fila = document.querySelector(".contenedor-carousel");
+    const flechaIzquierda = document.getElementById("flecha-izquierda");
+    const flechaDerecha = document.getElementById("flecha-derecha");
+    // console.log(fila.DOCUMENT_NODE);
 
     fetch(`${base_url}/movie/${movieID}?${api}`)
         .then((res) => res.json())
@@ -175,14 +193,15 @@ function getInfo(movieID) {
                     // Array que guardara un numero limitado de elementos del objeto cast
                     let arr_cast = [];
                     // Asignamos el tamaño del objeto cast en una variable para iterar
-                    let castSize = dataCast.cast.length
+                    let castSize = dataCast.cast.length;
                     // Iteramos el objeto cast y creamos la condicional que agrega solo diez elementos del objeto original
                     for (let j = 0; j < castSize && j < 10; j++) {
-                        var castName = dataCast.cast[j].name
-                        arr_cast.push(castName)
+                        var castName = dataCast.cast[j].name;
+                        arr_cast.push(castName);
                     }
+
                     // Asignamos como valor el array limitado a diez elementos, en una nueva variable para poder asignarla dentro del DOM
-                    //let arr_cast_global = arr_cast.join(", ")
+                    // arr_cast_global = arr_cast.join(", ")
                     //console.log(arr_cast_global)
 
                     // ASIGNAR VARIABLES CON DATOS
@@ -194,17 +213,17 @@ function getInfo(movieID) {
                     let movieDate = data.release_date;
                     let voteAverage = data.vote_average;
 
-                    let trailer = document.getElementById('youtube');
+                    let trailer = document.getElementById("youtube");
                     trailer.href = `https://www.youtube.com/results?search_query=${movieName}+trailer`;
 
                     let calificacion = document.getElementById(`movie_average`);
                     let average = document.getElementById(`average`);
 
-
                     // ASIGNAR VARIABLES (DATOS) EN HTML (DOM)
-                    //document.getElementById(`movie_cast`).innerHTML = arr_cast_global;
+                    // document.getElementById(`movie_cast`).innerHTML = arr_cast_global;
                     document.getElementById(`movie_name`).innerHTML = movieName;
-                    document.getElementById(`movie_original_name`).innerHTML = movieOriginalName;
+                    document.getElementById(`movie_original_name`).innerHTML =
+                        movieOriginalName;
                     document.getElementById(`movie_sinopsis`).innerHTML = movieSinopsis;
                     document.getElementById(`movie_poster_modal`).src = urlModal;
                     document.getElementById(`movie_date`).innerHTML = movieDate;
@@ -212,26 +231,18 @@ function getInfo(movieID) {
 
                     /* ------------------ CARRUSEL --------------------*/
 
-                    const movie_cast = document.getElementById("carousel");
+                    let htmlContentToAppend = "";
                     dataCast.cast.forEach((cast, index) => {
                         if (index < 10) {
-                            const link = document.createElement("a");
-                            link.className = "link_img";
-                            link.href = "#";
-                            const name = document.createElement("div")
-                            name.className = "text";
-                            name.innerText = cast.name;
-                            const poster = document.createElement("img");
-                            poster.className = "pelicula";
-                            var s2 = cast.id;
-                            poster.id = s2;
-                            poster.src = img_url + cast.profile_path;
-                            link.appendChild(poster);
-                            link.appendChild(name);
-                            movie_cast.appendChild(link);
+                            htmlContentToAppend += `
+                    <a class="link_img" href="#">
+                        <img class="pelicula" src="${img_url}${cast.profile_path}" alt="">
+                        <p class="text">${cast.name}</p>
+                    </a>
+            `;
                         }
                     });
-
+                    document.getElementById("carousel").innerHTML = htmlContentToAppend;
                     /*-------------------------------------------------*/
 
                     // CONDICION - ASIGNAR COLOR SEGUN CALIFICACION
@@ -261,62 +272,55 @@ function getInfo(movieID) {
                         average.classList.add("border-red");
                     }
                 });
-
-
         });
-
     // botones del carrusel -------------------------------------------
 
     // ? ----- ----- Event Listener para la flecha derecha. ----- -----
-    flechaDerecha.addEventListener('click', () => {
+    flechaDerecha.addEventListener("click", () => {
         fila.scrollLeft += fila.offsetWidth;
     });
 
     // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
-    flechaIzquierda.addEventListener('click', () => {
+    flechaIzquierda.addEventListener("click", () => {
         fila.scrollLeft -= fila.offsetWidth;
     });
     // ----------------------------------------------------------------
-
 }
 
-
 function pressSearch(event) {
-    if (event.key == 'Enter') {
-        let title = document.getElementById('search').value;
-        const alert = document.getElementById('alert');
-        const textAlert = document.getElementById('text_alert');
+    if (event.key == "Enter") {
+        let title = document.getElementById("search").value;
+        const alert = document.getElementById("alert");
+        const textAlert = document.getElementById("text_alert");
         if (movies.includes(title)) {
-            alert.style.backgroundColor = '#009900';
-            textAlert.innerText = 'EXITO: la pelicula se encuentra en el catalogo';
+            alert.style.backgroundColor = "#009900";
+            textAlert.innerText = "EXITO: la pelicula se encuentra en el catalogo";
         } else {
-            alert.style.backgroundColor = '#8E1900';
-            textAlert.innerText = 'ERROR: no se encontro la pelicula en el catalogo';
+            alert.style.backgroundColor = "#8E1900";
+            textAlert.innerText = "ERROR: no se encontro la pelicula en el catalogo";
         }
 
         setTimeout(function () {
-            alert.style.backgroundColor = '#11181e';
-            textAlert.innerText = '';
+            alert.style.backgroundColor = "#11181e";
+            textAlert.innerText = "";
         }, 2300);
     }
 }
 
 function buscar() {
-    const title = document.getElementById('search').value;
-    const alert = document.getElementById('alert');
-    const textAlert = document.getElementById('text_alert');
+    const title = document.getElementById("search").value;
+    const alert = document.getElementById("alert");
+    const textAlert = document.getElementById("text_alert");
     if (movies.includes(title)) {
-        alert.style.backgroundColor = '#009900';
-        textAlert.innerText = 'EXITO: la pelicula se encuentra en el catalogo';
+        alert.style.backgroundColor = "#009900";
+        textAlert.innerText = "EXITO: la pelicula se encuentra en el catalogo";
     } else {
-        alert.style.backgroundColor = '#8E1900';
-        textAlert.innerText = 'ERROR: no se encontro la pelicula en el catalogo';
+        alert.style.backgroundColor = "#8E1900";
+        textAlert.innerText = "ERROR: no se encontro la pelicula en el catalogo";
     }
 
     setTimeout(function () {
-        alert.style.backgroundColor = '#11181e';
-        textAlert.innerText = '';
+        alert.style.backgroundColor = "#11181e";
+        textAlert.innerText = "";
     }, 2300);
-
 }
-
